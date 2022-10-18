3b0cc0ca5596ea7093426665256854882b95e955
Lucas Moura Pereira <lucasmoura@ioasys.com.br>
2022-10-18 10:40:40 -0300

    gravar em json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import { client } from "./server/api";
 import { GET_REPOSITORIES } from "./graphql/schema";
 import { writeCSV } from "./modules/writeFile";
-import { writeJson } from './modules/writeJson';
+import { writeJson } from "./modules/writeJson";
 
-let variables = {
-  first: 100,
-  after: null,
+import _ from "lodash";
+
+const main = async () => {
+  let data = [];
+  let variables = {
+    first: 10,
+    after: null,
+  };
+  for (let i = 0; i < 10; i++) {
+    await client.request(GET_REPOSITORIES, variables).then(({ search }) => {
+      data.push(search.nodes);
+      writeCSV(search.nodes);
+      variables = {
+        first: 10,
+        after: search.pageInfo.endCursor,
+      };
+    });
+  }
+  writeJson(_.concat(data));
 };
 
-client.request(GET_REPOSITORIES, variables).then(({ search }) => {
-  writeCSV(search.nodes);
-  writeJson(search.nodes);
-});
+main();
--- src/modules/writeJson.js
+++ src/modules/writeJson.js
@@ -2,9 +2,15 @@ import fs from "fs";
 
 export const writeJson = async (nodes) => {
   let file = "./src/out/lab03.json";
-  fs.writeFile(file, JSON.stringify(nodes, null, "\t"), (err) => {
-    if (err) return err;
-
-    console.log("Finished writing data");
-  });
+  fs.writeFile(
+    file,
+    JSON.stringify(nodes, null, "\t"),
+    {
+      flag: "a",
+    },
+    (err) => {
+      if (err) return err;
+      console.log("Finished writing data");
+    }
+  );
 };
